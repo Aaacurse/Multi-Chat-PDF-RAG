1e5266beb848393041910b090c08541e5083842e
Aaacurse <rajputaakarsh2004@gmail.com>
2026-02-24 00:02:25 +0530

    test file

---
+++ tests/test_config.py
@@ -0,0 +1,9 @@
+import pytest
+from config import *
+
+def test_configfile():
+    assert HUGGINGFACEHUB_API_TOKEN is not None
+    assert 0<=LLM_TEMP<=1
+    assert TOP_K_RESULTS>0
+    assert CHUNK_OVERLAP>0
+    assert CHUNK_SIZE>0
